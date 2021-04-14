3a369fc5983483c229e8fbeef3976e8dc7ad260c
Frederik Flint <frederikflint@gmail.com>
2021-04-14 13:42:34 +0200

    Cleanup

--- MultiAgent/SearchClient/Position.cs
+++ MultiAgent/SearchClient/Position.cs
@@ -12,6 +12,22 @@ namespace MultiAgent.searchClient
             Col = col;
         }
 
+        public Position(Position position)
+        {
+            Row = position.Row;
+            Col = position.Col;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Position position)
+            {
+                return this.Row == position.Row && this.Col == position.Col;
+            }
+
+            return false;
+        }
+
         public override int GetHashCode()
         {
             return HashCode.Combine(Row, Col);
