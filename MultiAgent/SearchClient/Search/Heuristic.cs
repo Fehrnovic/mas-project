54f321e2edbdacdcaa019b7e0b90941df23f8e10
Oscar Fehrn <oscarfehrn@gmail.com>
2021-05-05 10:40:05 +0200

    Heuristic wip

--- MultiAgent/SearchClient/Search/Heuristic.cs
+++ MultiAgent/SearchClient/Search/Heuristic.cs
@@ -39,7 +39,6 @@ namespace MultiAgent.SearchClient.Search
                     goalScore += 100;
                 }
 
-                
                 foreach (var box in state.PositionsOfBoxes.Where(b => b.Value.Letter == goal.Letter))
                 {
                     // Box placed correctly already
@@ -58,48 +57,167 @@ namespace MultiAgent.SearchClient.Search
 
         public static int CalculateHeuristicMA(MAState state)
         {
-            var agentDistance = 0;
+            // Calculate goal count heuristic
+            int goalCount = HGoalCount(state);
 
+            // Calculate manhattan distance / actual distance
+            int distance = HDistance(state);
+
+            // Calculate manhattan distance / actual distance
+            int actionPenalty = HActionPenalty(state);
+
+            // Weigh goal count heuristic higher
+            return (100000 * goalCount) + distance + actionPenalty;
+        }
+
+        private static int HGoalCount(MAState state)
+        {
+            var boxGoalScore = 0;
+
+            foreach (var boxGoal in state.BoxGoals)
+            {
+                if (state.BoxAt(boxGoal.GetInitialLocation()) != null)
+                {
+                    // Box goal is complete
+                    if (state.BoxAt(boxGoal.GetInitialLocation()).Letter == boxGoal.Letter)
+                    {
+                        // Box is placed correctly
+                        continue;
+                    }
+
+                    // Wrong box is placed on the goal
+                    boxGoalScore += 100;
+                }
+                else
+                {
+                    // Goal is not complete
+                    boxGoalScore += 1;
+                }
+            }
+
+            var agentGoalScore = 0;
+            //foreach (var agentGoal in state.AgentGoals)
+            //{
+            //    var agentAtGoal = state.PositionsOfAgents[agentGoal.GetInitialLocation()];
+            //    if (agentAtGoal != null)
+            //    {
+            //        // Agent goal is complete
+            //        if (agentAtGoal.Number == agentGoal.Number)
+            //        {
+            //            // Agent is placed correctly
+            //            continue;
+            //        }
+
+            //        // Wrong agent is placed on the goal
+            //        agentGoalScore += 2;
+            //    }
+            //    else
+            //    {
+            //        // Agent goal is not complete
+            //        agentGoalScore += 1;
+            //    }
+            //}
+
+            return boxGoalScore + agentGoalScore;
+
+            //// Calculate distance between all boxes
+            //foreach (var boxKeyValuePair in state.PositionsOfBoxes.Where(b => b.Value.Letter == goal.Letter))
+            //{
+            //    // Box placed correctly already
+            //    if (state.BoxGoals.Exists(g => g.Letter == boxKeyValuePair.Value.Letter && g.GetInitialLocation().Equals(boxKeyValuePair.Key)))
+            //    {
+            //        // Disregard box
+            //        continue;
+            //    }
+
+            //    goalScore += Level.DistanceBetweenPositions[(goal.GetInitialLocation(), boxKeyValuePair.Key)];
+            //}
+        }
+
+        private static int HDistance(MAState state)
+        {
+            // Get distance of closest box to boxGoal, and closest agent to the box
+            var distance = 0;
+            foreach (var boxGoal in state.BoxGoals)
+            {
+                // Find closest box to boxGoal
+                var (boxPosition, box) = state.PositionsOfBoxes
+                    .Where(kvp => kvp.Value.Letter == boxGoal.Letter)
+                    .OrderBy(kvp => Level.DistanceBetweenPositions[(kvp.Key, boxGoal.GetInitialLocation())])
+                    .First();
+
+                var boxToGoalDistance = Level.DistanceBetweenPositions[(boxPosition, boxGoal.GetInitialLocation())];
+
+                // Find position of agent closest to box
+                var closestAgentPosition = state.PositionsOfAgents
+                    .Where(kvp => kvp.Value.Color == box.Color)
+                    .OrderBy(kvp => Level.DistanceBetweenPositions[(kvp.Key, boxPosition)])
+                    .First().Key;
+
+                var agentToBoxDistance = Level.DistanceBetweenPositions[(closestAgentPosition, boxPosition)];
+
+                distance += boxToGoalDistance + agentToBoxDistance;
+            }
+
+            var agentToGoalDistance = 0;
+
+            // If all boxGoals for an agent are completed, then we calculate the distance to his goal
             foreach (var agent in state.Agents)
             {
-                var agentGoal = state.AgentGoals.FirstOrDefault(ag => ag.Number == agent.Number);
-                if (agentGoal != null)
+                // Find all the agent's boxes
+                var boxGoals = state.BoxGoals.Where(bg => bg.Color == agent.Color).ToList();
+
+                var agentGoalsSatisfied = true;
+
+                if (boxGoals.Count > 0)
                 {
-                    var agentPosition = state.AgentPositions[agent];
-                    agentDistance += Level.DistanceBetweenPositions[(agentPosition, agentGoal.GetInitialLocation())];
+                    foreach (var boxGoal in boxGoals)
+                    {
+                        state.PositionsOfBoxes.TryGetValue(boxGoal.GetInitialLocation(), out var box);
+                        if (box == null || box.Letter != boxGoal.Letter)
+                        {
+                            agentGoalsSatisfied = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (agentGoalsSatisfied)
+                {
+                    var agentGoal = state.AgentGoals.FirstOrDefault(ag => ag.Number == agent.Number);
+                    if (agentGoal != null)
+                    {
+                        agentToGoalDistance +=
+                            Level.DistanceBetweenPositions[
+                                (state.AgentPositions[agent], agentGoal.GetInitialLocation())];
+                    }
                 }
             }
 
-            var goalScore = 0;
+            return distance + agentToGoalDistance;
+        }
 
-            foreach (var goal in state.BoxGoals)
+        private static int HActionPenalty(MAState state)
+        {
+            if (state.JointActions == null)
             {
-                // Goal is complete
-                if (state.BoxAt(goal.GetInitialLocation()) != null)
-                {
-                    if (state.BoxAt(goal.GetInitialLocation()).Letter == goal.Letter)
-                    {
-                        continue;
-                    }
+                return 0;
+            }
 
-                    goalScore += 100;
+            var actionPenalty = 0;
+            foreach (var action in state.JointActions.Values)
+            {
+                if (action.Type == ActionType.NoOp)
+                {
+                    actionPenalty += 0;
                 }
-
-
-                foreach (var box in state.PositionsOfBoxes.Where(b => b.Value.Letter == goal.Letter))
+                else
                 {
-                    // Box placed correctly already
-                    if (state.BoxGoals.Exists(g => g.Letter == box.Value.Letter && g.GetInitialLocation().Equals(box.Key)))
-                    {
-                        // Disregard box
-                        continue;
-                    }
-
-                    goalScore += Level.DistanceBetweenPositions[(goal.GetInitialLocation(), box.Key)];
+                    actionPenalty += 1;
                 }
             }
 
-            return agentDistance + goalScore;
+            return actionPenalty;
         }
     }
 }
