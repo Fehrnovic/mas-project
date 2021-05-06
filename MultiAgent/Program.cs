996d8face9f33698ef5922297f95130933a423d7
Emil Godsk <emil.godsk@gmail.com>
2021-05-06 23:59:10 +0200

    run pacman

--- MultiAgent/Program.cs
+++ MultiAgent/Program.cs
@@ -33,7 +33,7 @@ namespace MultiAgent
             Timer.Start();
 
             // Initialize the level
-            Level.ParseLevel("MAsimple5.lvl");
+            Level.ParseLevel("MApacman.lvl");
 
             Console.Error.WriteLine($"Level initialized in {Timer.ElapsedMilliseconds / 1000.0} seconds");
 
